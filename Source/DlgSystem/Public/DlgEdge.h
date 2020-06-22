81a1a24dbb0f9b4ae0386d58bc411c78b90de980
Daniel Butum <danibutum@gmail.com>
2020-06-22 22:42:27 +0300

    Squashed 'Plugins/DlgSystem/' changes from 9b7811020..cf113a5ca
    cf113a5ca Merge branch '4.22' into 4.23
    ae717bfab Update example Content
    8e6c2e107 [C] Some more name cleaning
    a2e743315 [C] More error messages improvement
    27f068a8b [C] Better Error messages for Events
    173ab674e [C] Improve custom conditions errors and clean some code
    8ae81a87b [C] Some API cleanup, use references instead of pointers where we can
    8fba0fe99 [C] WIP Context and Dialogue Manager have better error messages | Relax our requirement for the participants, just warn about extra participant names that are not in the Dialogue
    cc767ce24 [C] Improve the API consistency and error messages of the Dialogue Context and Dialogue Manager
    671db137d [C] Fix edge now showing when creating a new node | Some code cleanup of NodePos
    428cbc633 [C] More code style cleanup
    4b366a281 [C] Code style cleanup
    76ca44c6d Advance version to 9.1, write WIP changelog
    9261509eb [C] Get rid of deprecated functions and just do a config FunctionRedirect
    25ab3bb31 [C] Make everything more blueprint friendly (same level of access as C++) | Fix categories names
    bfb206f58 [G] Remove DlgContextInternal and use only DlgContext
    0c271b699 [C] Fix wrong error messages when condition does not need a participant
    f60621c26 Merge branch '4.22' into 4.23
    c88e03bb0 [G] Clean unused serialize code | Fix Edges objects outer (this fixes the custom conditions in non editor builds)
    0eb0297c0 [C] Fix wrong warning when refreshing data when second participant is not involved
    2c710de0a [C] Add virtual specifier to EnterEvent_Implementation in order to override custom events behavior on C++ side.
    9f116a595 Merge branch 'patch-1' into '4.22'
    cad2abb38 Add virtual specifier to IsConditionMet_Implementation in order to override custom condition behavior on C++ side.
    
    git-subtree-dir: Plugins/DlgSystem
    git-subtree-split: cf113a5cad3b64529933df0d97ecf165bdf35c6c

--- Source/DlgSystem/Public/DlgEdge.h
+++ Source/DlgSystem/Public/DlgEdge.h
@@ -17,17 +17,27 @@ class UDlgDialogue;
 /**
  * The representation of a child in a node. Defined by a TargetIndex which points to the index array in the Dialogue.Nodes
  */
-USTRUCT(BlueprintType, Blueprintable)
+USTRUCT(BlueprintType)
 struct DLGSYSTEM_API FDlgEdge
 {
 	GENERATED_USTRUCT_BODY()
 
 public:
+	// Creates a simple edge without text, without conditions
+	FDlgEdge(int32 InTargetIndex = INDEX_NONE) : TargetIndex(InTargetIndex) {}
+
+	//
+	// ICppStructOps Interface
+	//
+
 	bool operator==(const FDlgEdge& Other) const
 	{
 		return TargetIndex == Other.TargetIndex &&
-			Text.EqualTo(Other.Text) &&
-			Conditions == Other.Conditions;
+            SpeakerState == Other.SpeakerState &&
+            Text.EqualTo(Other.Text) &&
+            bIncludeInAllOptionListIfUnsatisfied == Other.bIncludeInAllOptionListIfUnsatisfied &&
+            TextArguments == Other.TextArguments &&
+            Conditions == Other.Conditions;
 	}
 
 	bool operator!=(const FDlgEdge& Other) const
@@ -35,32 +45,30 @@ public:
 		return !(*this == Other);
 	}
 
-	// Operator overload for serialization
-	friend FArchive& operator<<(FArchive &Ar, FDlgEdge& DlgEdge);
-
-	/** Creates a simple edge without text, without conditions */
-	FDlgEdge(int32 InTargetIndex = INDEX_NONE) : TargetIndex(InTargetIndex) {}
+	//
+	// Own methods
+	//
 
 	// Is the Text property visible on this edge, the edges comes from the ParentNode
-	static bool IsTextVisible(const UDlgNode* ParentNode);
+	static bool IsTextVisible(const UDlgNode& ParentNode);
 
 	// Updates the text value of the Edge Text from the default value and text remapping (if any)
 	void UpdateTextValueFromDefaultAndRemapping(
-		const UDlgDialogue* ParentDialogue, const UDlgNode* ParentNode, const UDlgSystemSettings* Settings, bool bUpdateFromRemapping
+		const UDlgDialogue& ParentDialogue, const UDlgNode& ParentNode, const UDlgSystemSettings& Settings, bool bUpdateFromRemapping
 	);
 
 	// Updates the namespace or keys depending on the settings
-	void UpdateTextsNamespacesAndKeys(const UObject* ParentObject, const UDlgSystemSettings* Settings);
+	void UpdateTextsNamespacesAndKeys(const UObject* ParentObject, const UDlgSystemSettings& Settings);
 
 	// Rebuilds TextArguments
 	void RebuildTextArguments() { FDlgTextArgument::UpdateTextArgumentArray(Text, TextArguments); }
 	void RebuildTextArgumentsFromPreview(const FText& Preview) { FDlgTextArgument::UpdateTextArgumentArray(Preview, TextArguments); }
 
-	/** Returns with true if every condition attached to the edge and every enter condition of the target node are satisfied */
-	bool Evaluate(const UDlgContext* Context, TSet<const UDlgNode*> AlreadyVisitedNodes) const;
+	// Returns with true if every condition attached to the edge and every enter condition of the target node are satisfied //
+	bool Evaluate(const UDlgContext& Context, TSet<const UDlgNode*> AlreadyVisitedNodes) const;
 
-	/** Constructs the ConstructedText. */
-	void RebuildConstructedText(const UDlgContext* Context, FName NodeOwnerName);
+	// Constructs the ConstructedText.
+	void RebuildConstructedText(const UDlgContext& Context, FName FallbackParticipantName);
 
 	const TArray<FDlgTextArgument>& GetTextArguments() const { return TextArguments; }
 
@@ -92,46 +100,56 @@ public:
 	const FText& GetUnformattedText() const { return Text; }
 	FText& GetMutableUnformattedText() { return Text; }
 
-	/** Returns if the Edge is valid, has the TargetIndex non negative  */
-	bool IsValid() const
+	// Returns if the Edge is valid, has the TargetIndex non negative
+	bool IsValid() const { return TargetIndex > INDEX_NONE; }
+
+	static const FDlgEdge& GetInvalidEdge()
 	{
-		return TargetIndex > INDEX_NONE;
+		static FDlgEdge DlgEdge;
+		return DlgEdge;
 	}
 
-	static const FDlgEdge& GetInvalidEdge();
-
-	/** Helper functions to get the names of some properties. Used by the DlgSystemEditor module. */
+	// Helper functions to get the names of some properties. Used by the DlgSystemEditor module.
 	static FName GetMemberNameText() { return GET_MEMBER_NAME_CHECKED(FDlgEdge, Text); }
 	static FName GetMemberNameTextArguments() { return GET_MEMBER_NAME_CHECKED(FDlgEdge, TextArguments); }
 
 public:
-	/** Index of the node in the Nodes TArray of the dialogue this edge is leading to */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DialogueEdgeData, Meta = (ClampMin = -1))
+	// Index of the node in the Nodes TArray of the dialogue this edge is leading to
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Dialogue|Edge", Meta = (ClampMin = -1))
 	int32 TargetIndex = INDEX_NONE;
 
-	/** Required but not sufficient conditions - target node's enter conditions are checked too */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = DialogueEdgeData)
+	// Required but not sufficient conditions - target node's enter conditions are checked too
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue|Edge")
 	TArray<FDlgCondition> Conditions;
 
-	/** player emotion/state attached to this player choice */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = DialogueEdgeData)
+	// Player emotion/state attached to this player choice
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue|Edge")
 	FName SpeakerState;
 
-	/** Set this to false in order to skip this edge in the AllChildren array (which lists both satisfied and not satisfied player choices */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = DialogueEdgeData)
+	// Set this to false in order to skip this edge in the AllChildren array (which lists both satisfied and not satisfied player choices
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue|Edge")
 	bool bIncludeInAllOptionListIfUnsatisfied = true;
 
 protected:
 	// Some Variables are here to stop misuse
 
-	/** Text associated with the child, can be used for user choices */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = DialogueEdgeData, Meta = (MultiLine = true))
+	// Text associated with the child, can be used for user choices
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue|Edge", Meta = (MultiLine = true))
 	FText Text;
 
 	// If you want replaceable portions inside your Text nodes just add {identifier} inside it and set the value it should have at runtime.
-	UPROPERTY(EditAnywhere, EditFixedSize, Category = DialogueEdgeData)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, EditFixedSize, Category = "Dialogue|Edge")
 	TArray<FDlgTextArgument> TextArguments;
 
-	/** Constructed at runtime from the original text and the arguments if there is any.*/
+	// Constructed at runtime from the original text and the arguments if there is any.
 	FText ConstructedText;
+};
+
+template<>
+struct TStructOpsTypeTraits<FDlgEdge> : public TStructOpsTypeTraitsBase2<FDlgEdge>
+{
+	enum
+	{
+		WithIdenticalViaEquality = true
+    };
 };
